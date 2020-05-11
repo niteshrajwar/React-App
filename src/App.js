0aa154e51b332298b639234a56f6ae03ced41935
niteshrajwar <rajwar.nitesh01@gmail.com>
2020-05-12 01:15:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import Header from './components/Header'
 import Login from './components/Login'
+import Users from './components/Users'
 import './App.css'
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
      <Switch>
      <Route path="/login" component={Login} />
      </Switch>
+     <Users />
      </Router>
     // </div>
   );
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
 
 function Users() {
+   const [users,setUsers] = useState([]);
+
+    useEffect(() => {
+      fetch('https://reqres.in/api/users')
+      .then(res=> res.json())
+      .then(response =>{
+          if(response && response.data) {
+              setUsers(response.data)
+          }
+      })
+    }, [])
     return (
         <div>
-            
+           {users.length > 0 ? <div> Users List <ul> {users.map(us => <li key={us.id}>{us.first_name} {us.last_name}</li>)} </ul></div> : null} 
         </div>
     )
 }
