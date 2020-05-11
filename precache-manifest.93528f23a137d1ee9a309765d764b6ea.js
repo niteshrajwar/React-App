fdcc4dc11f00895400b3ebce09b714b1fd622771
niteshrajwar <rajwar.nitesh01@gmail.com>
2020-05-12 01:04:22 +0530

    Updates

---
+++ precache-manifest.93528f23a137d1ee9a309765d764b6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bcc373f0ccd185ed2c456e59ccb0ca",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "678044642ced67690ef2",
+    "url": "/React-App/static/css/main.87a496cb.chunk.css"
+  },
+  {
+    "revision": "6ad6e2c2963846f93fc5",
+    "url": "/React-App/static/js/2.72bba7a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-App/static/js/2.72bba7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678044642ced67690ef2",
+    "url": "/React-App/static/js/main.5f440458.chunk.js"
+  },
+  {
+    "revision": "3449e3065147793a20c4",
+    "url": "/React-App/static/js/runtime-main.a8433dc8.js"
+  }
+]);
